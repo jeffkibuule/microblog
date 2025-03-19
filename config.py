7571d2b99a27a90a1b7e97ea6defb5b1731da375
Jeff Kibuule <jeff.kibuule@outlook.com>
2025-03-18 21:48:43 -0700

    Complete Chapter 10

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class Config:
 	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	ADMINS = ['jeff@example.com']
+	ADMINS = ['jeffkibuule@fastmail.com']
 
 	POSTS_PER_PAGE = 25
